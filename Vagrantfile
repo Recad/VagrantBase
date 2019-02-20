4ba7aa54c859cc7295c7c3b495eeba195d9933e6
recad <dacer-133@hotmail.com>
2019-02-20 13:09:07 -0500

    nuevas opciones para NFS-server

--- Vagrantfile
+++ Vagrantfile
@@ -10,17 +10,29 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
   #modidficaciones desde el provider (en este caso virtual box)---------
   config.vm.provider :virtualbox do |vb|
-	vb.customize [ 'modifyvm', :id, '--name', 'practica' ]
+	vb.customize [ 'modifyvm', :id, '--name', 'SERVIDOR-NFS' ]
   end
   #compartir folder-----------------------------------------------------
-  config.vm.synced_folder ".", "/tmp"
+  #config.vm.synced_folder ".", "/home"
   
   #compartir puerto-ejemplo el 80 de la maquina virtual al 8080 del host
-  config.vm.network "forwarded_port", guest: 80, host: 8080
+  #config.vm.network "forwarded_port", guest: 80, host: 8085
+  
+  
+  ##Bridge para la vm 
+  #config.vm.network "public_network", :bridge => "eth0", ip: "xxx.yyy.zzz.153", :netmask => "255.255.255.128", auto_config: false
+  config.vm.network :public_network, :bridge => "en0: Ethernet"
+  
   
   #aprovisionar con script----------------------------------------------
   config.vm.provision "shell", path: "script.sh"
-
+  #aprovisionar con vagrant para docker---------------------------------
+  #config.vm.provision "docker" do |d|
+  #  d.pull_images "postgres"
+  #  d.run "postgres",
+  #  args: "-v '/home:/var/lib/postgresql/data' -p '5432:5432' -e 'POSTGRES_PASSWORD=docker' -d"
+    
+  #end
 
 
 end
--- script.sh
+++ script.sh
@@ -1,22 +1,73 @@
 
 #!/usr/bin/env bash
 sudo apt-get update
-#docker
-sudo sudo apt-get install docker-ce docker-ce-cli containerd.io --fix-missing
-sudo sudo apt-get install docker-ce docker-ce-cli containerd.io --fix-missing
-#apache
-sudo apt-get -y install apache2 --fix-missing
-sudo apt-get -y install apache2 --fix-missing
-#htop
-sudo apt-get -y install htop --fix-missing
-sudo apt-get -y install htop --fix-missing
-#nano
-sudo apt-get -y install nano --fix-missing
-sudo apt-get -y install nano --fix-missing
+##Activar user namespaces-----------------------------------------------
+#sudo sysctl -w kernel.unprivileged_userns_clone=1
 
-#pull imagen
-sudo docker pull postgres
+##docker manual---------------------------------------------------------
+#yes | sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository \
+#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) \
+#   stable"
+#sudo apt-get update
+#yes | sudo apt-get install docker-ce=17.06.0~ce-0~ubuntu docker-ce-cli=17.06.0~ce-0~ubuntu containerd.io
+#sudo sudo apt-get install docker-ce docker-ce-cli containerd.io --fix-missing
 
-sudo docker run -p 5432:5432 -v /tmp:/var/lib/postgresql/data postgres
+#sudo docker pull imagen manual
+#sudo docker pull postgres
 
+#sudo docker run -d -p 5432:5432 -v /home:/var/lib/postgresql/data postgres
+##Se cierra configuracion para docker--------------------------------------------------------------------------------
 
+##apache------------------------------------------------------------------------------------------------
+#sudo apt-get -y install apache2 --fix-missing
+#sudo apt-get -y install apache2 --fix-missing
+
+##htop-------------------------------------------------------------------------------------------------
+#sudo apt-get -y install htop --fix-missing
+#sudo apt-get -y install htop --fix-missing
+
+##nano-------------------------------------------------------------------------------------------------
+#sudo apt-get -y install nano --fix-missing
+#sudo apt-get -y install nano --fix-missing
+
+##CONFIGURACION PARA CLUSTES Y NFS-------------------------------------------------------------------
+
+##PARA SERVER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+sudo apt-get update
+yes | sudo apt-get install nfs-kernel-server
+##PARA CLIENT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#sudo apt-get update
+#sudo apt-get install nfs-common
+#crear carpeta y ver sus usuarios y permisos
+#sudo mkdir /home/nfs/general -p
+#ls -la /var/nfs/general  #Esto no es necesario pero ayuda 
+#configurando usuario y grupo para la carpeta
+#sudo chown nobody:nogroup /home/nfs/general
+
+##Archivo de config
+
+echo "// Ejemplo de archivo /etc/exports de configuración del servidor NFS:
+
+# Compartir la carpeta home del servidor
+# en modo lectura y escritura y accesible desde la red 192.168.0.0/24
+/home 192.168.0.0/255.255.255.0(rw)
+
+# Compartir carpeta tmp a todos como 'solo-lectura'
+/tmp *(ro)
+
+# Compartir carpeta /var/log a un PC como 'solo-lectura'
+/var/log 192.168.0.211(ro) 
+" >> /etc/exports
+
+sudo /etc/init.d/portmap start
+
+##INSTALAR MPI
+yes | sudo apt-get install libcr-dev mpich2 mpich2-doc
